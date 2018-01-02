541cc5667b8afab5125ff0441a26f6995e0e33bc
DeerisLeGris <yoshi_gris@outlook.com>
2018-01-02 18:47:02 +0100

    Save module fixed

--- SerrisCodeEditor/SerrisModulesServer/SystemModules/4/main.js
+++ SerrisCodeEditor/SerrisModulesServer/SystemModules/4/main.js
@@ -24,5 +24,5 @@ function saveas()
 
 function onEditorViewReady()
 {
-    sceelibs.editorEngine.injectJS("editor.addAction({ id: 'save_button', label: 'Save', contextMenuGroupId: 'sce', keybindings: [ monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S ], contextMenuOrder: 1.5, run: function(ed) { sceelibs.editorEngine.saveCurrentSelectedTab(); var tab = sceelibs.tabsManager.getTabViaID(sceelibs.tabsManager.getCurrentSelectedTabAndTabsListID()); if (tab.pathContent === \"\") { tab.createTabFile(); } tab.saveContentToFile(); sceelibs.consoleManager.sendConsoleInformationNotification('\"' + tab.tabName + '\" has been saved at ' + tab.pathContent + ' !'); return null; } });");
+    sceelibs.editorEngine.injectJS("editor.addAction({ id: 'save_button', label: 'Save', contextMenuGroupId: 'sce', keybindings: [ monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S ], contextMenuOrder: 1.5, run: function(ed) { sceelibs.editorEngine.saveCurrentSelectedTab(); var tab = sceelibs.tabsManager.getTabViaID(sceelibs.tabsManager.getCurrentSelectedTabAndTabsListID()); tab.saveContentToFile(); sceelibs.consoleManager.sendConsoleInformationNotification('\"' + tab.tabName + '\" has been saved at ' + tab.pathContent + ' !'); return null; } });");
 }
