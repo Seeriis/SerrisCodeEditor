1e6576c4a61b248be60b826146459503005bbb2b
DeerisLeGris <yoshi_gris@outlook.com>
2018-08-09 19:52:51 +0200

    New build name + new assets (thanks to the new logo of SCE :) )

--- SerrisCodeEditor/SCEELibs/SCEInfos.cs
+++ SerrisCodeEditor/SCEELibs/SCEInfos.cs
@@ -5,10 +5,11 @@
     public static class SCEInfos
     {
         //SCE Version information
-        public static string versionName { get => "Torcy build"; }
-        public static string versionNumber { get => "1.4"; }
+        public static string versionName { get => "Val d'Europe build"; }
+        public static string versionNumber { get => "1.5"; }
 
         /*History: 
+         * Val d'Europe build ("1.5")
          * Torcy build ("1.4")
          * Bry sur Marne build ("1.3")
          * Neuilly Plaisance build ("1.2")
